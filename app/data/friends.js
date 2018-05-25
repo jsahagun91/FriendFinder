96225e11cb061e9157a44f4c96f04e71585a5fcc
Jose Sahagun <JoseSahagun@Joses-MacBook-Pro-2.local>
2018-05-24 18:36:09 -0700

    add hw

---
+++ app/data/friends.js
@@ -0,0 +1,42 @@
+// DATA
+// Below datawill hold all friends data.
+
+var friendsData = [
+    {
+        name: "Han Solo",
+        photo: "https://upload.wikimedia.org/wikipedia/en/thumb/b/be/Han_Solo_depicted_in_promotional_image_for_Star_Wars_%281977%29.jpg/220px-Han_Solo_depicted_in_promotional_image_for_Star_Wars_%281977%29.jpg",
+        scores: [
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1
+        ]
+    },
+
+    {
+        name: "Princess Leia",
+        photo: "https://images.halloweencostumes.com/products/42513/1-1/secret-wishes-princess-leia-wig.jpg",
+        scores: [
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5
+        ]
+    }
+
+];
+
+
+module.exports = friendsData;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,57 @@
+//===============================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+//===============================================
+
+var friendsData = require("../data/friends.js");
+
+//=============================================== 
+// Routing 
+//===============================================
+
+module.exports = function(app) {
+    // API GET Requests
+    // Below code handles when users "visit" a page.
+    // In each of the belowe cases when a user visits a link
+    // (ex: localhost:PORT/api/admin..) they are shown a JSON of the data in the table
+    //-------------------------------------------
+
+    app.get("/api/friends", function(req, res) {
+        res.json(friendsData);
+      });
+
+
+    app.post("/api/friends", function(req, res) {
+        // req.body is available since we're using the body-parser middleware.
+            var arrayScores = req.body.scores
+            var arrayCompare = []
+            var totalScore = 0
+            for (i = 0 ; i < friendsData.length ; i++ ) {
+                
+                for (j = 0 ; j < 10 ; j++) {
+                   totalScore += Math.abs(friendsData[i].scores[j] - arrayScores[j])
+                   console.log(totalScore)
+                }
+                arrayCompare.push(totalScore)
+                totalScore = 0
+                
+            }
+            var matchIndex = indexOfSmallest(arrayScores)
+            console.log(arrayCompare)
+            console.log(arrayScores)
+            res.json(friendsData[indexOfSmallest(arrayScores)]);
+            //friendsData.push(req.body);
+            console.log(matchIndex)
+});
+
+       
+};
+
+        function indexOfSmallest(a) {
+            var lowest = 0;
+            for (var i = 1; i < a.length; i++) {
+            if (a[i] < a[lowest]) lowest = i;
+            }
+            return lowest;
+        } 
